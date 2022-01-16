7c6041f40be4f06d7176eef56e811bd0cacb0772
dimplekollipara <dimple.kollipara261@gmail.com>
2022-01-16 15:51:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-x1pmyp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-x1pmyp)
---
+++ index.js
@@ -0,0 +1,5 @@
+// Import stylesheets
+import './style.css';
+
+const el = document.querySelector('.abc');
+el.innerHTML = '<p>Welcome to Javascript</p>';
